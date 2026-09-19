a220c4420237d97b849f23f5db6c72614a9fbaab
vaibhavdraws <vaibhavdraws8@gmail.com>
2026-09-19 17:58:20 +0530

    Prepare backend for Render

--- server/server.js
+++ server/server.js
@@ -321,15 +321,14 @@ app.post("/admin-login", (req, res) => {
   })
 
 })
-
 /* =========================
    START SERVER
 ========================= */
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 5000, '0.0.0.0', () => {
 
-  console.log(
-    `Server running on port ${process.env.PORT}`
-  )
+    console.log(
+        `Server running on port ${process.env.PORT || 5000}`
+    )
 
 })
